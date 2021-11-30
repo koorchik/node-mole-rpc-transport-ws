71aa48a59e02b6ab5e0bbc136b0b71d1c21b578a
artemtrush <atrush.unit@gmail.com>
2021-11-30 18:05:05 +0200

    Update wss server transport to the same style as ws

--- TransportServerWSS.js
+++ TransportServerWSS.js
@@ -1,17 +1,21 @@
-class TransportServerWS {
+class TransportServerWSS {
     constructor({ wss } = {}) {
         if (!wss) throw new Error('"wss" required');
+
         this.wss = wss;
     }
 
     async onData(callback) {
         this.wss.on('connection', ws => {
-            ws.on('message', async reqData => {
-                const resData = await callback(reqData);
-                if (!resData) return;
+            const messageHandler = async (data) => {
+                const response = await callback(data);
 
-                ws.send(resData);
-            });
+                if (response) {
+                    ws.send(response);
+                }
+            };
+
+            ws.on('message', messageHandler);
 
             ws.on('close', () => {
                 ws.terminate();
@@ -20,4 +24,4 @@ class TransportServerWS {
     }
 }
 
-module.exports = TransportServerWS;
+module.exports = TransportServerWSS;
